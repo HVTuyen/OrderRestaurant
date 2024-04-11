a0a671c02bc9d6d91f63791645501ceafaa20a0c
Tuyến Hồ <hovantuyen2002@gmail.com>
2024-04-11 09:08:00 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {StoreProvider} from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <StoreProvider>
+        <App />
+      </StoreProvider>
+    </Router>
   </React.StrictMode>
 );
 
